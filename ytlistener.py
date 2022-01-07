f46a0df8635cb5860f23fc68f305ffa911a11591
John Madon <john@unwisegeek.net>
2022-01-07 18:50:41 -0500

    Fixed MQTT, added logging, and added service to MQTT dictionary

--- ytlistener.py
+++ ytlistener.py
@@ -4,6 +4,7 @@ import requests
 import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
 import sys
+from time import sleep
 from config import (
     MQTT_HOST,
     MQTT_PORT,
@@ -16,6 +17,7 @@ from config import (
 def mqtt_on_connect(mqttc, userdata, flags, rc):
         print(f"Connected to MQTT with result code {str(rc)}")
         mqttc.subscribe("twchat")
+        print("Subscribed to Twitch Chat")
 
 def mqtt_on_message(mqttc, userdata, msg):
     payload = eval(msg.payload.decode('utf-8'))
@@ -23,12 +25,13 @@ def mqtt_on_message(mqttc, userdata, msg):
         msg = f"[Twitch] {payload['author']}: {payload['msg']}"
     else:
         msg = f"{payload['msg']}"
-    chat_obj.send_message(msg, livechat_id)
+    print(f"Sending: {msg}")
+    chatOut.send_message(msg, livechat_id)
     
 def mqtt_publish(author, msg):
     publish.single(
             'ytchat', 
-            str(dict(author=author, msg=msg)),
+            str(dict(author=author, msg=msg, service="Youtube")),
             qos=0, 
             retain=False, 
             hostname=MQTT_HOST,
@@ -45,8 +48,9 @@ def mqtt_publish(author, msg):
 
 try: 
     livechat_id = youtubechat.get_live_chat_id_for_stream_now("oauth_creds")
-    chat_obj = youtubechat.YouTubeLiveChat("oauth_creds", [livechat_id])
-    chat_obj.start()
+    chatOut = youtubechat.YoutubeLiveChat("oauth_creds", [livechat_id])
+    chatOut.start()
+    print("Started chatOut")
 except youtubechat.ytchat.YoutubeLiveChatError:
     print("You blew the budget. Listening only.")
     mqtt_publish("None", "YouTube listener in listen mode only. No messages will be shared from Twitch to YouTube.")
@@ -55,6 +59,18 @@ except youtubechat.ytchat.YoutubeLiveChatError:
 mqttc = mqtt.Client()
 mqttc.on_connect = mqtt_on_connect
 mqttc.on_message = mqtt_on_message
+if MQTT_AUTH == None:
+    mqttc.username_pw_set(
+        username=None, 
+        password=None,
+        )
+else:
+    mqttc.username_pw_set(
+        username=MQTT_AUTH["username"],
+        password=MQTT_AUTH["password"]
+        )
+mqttc.connect(MQTT_HOST, MQTT_PORT, 15)
+mqttc.loop_start()
 
 try:
     vid = sys.argv[1]
@@ -62,18 +78,19 @@ except IndexError:
     vid = input("Please enter the video ID: ")
 
 try:
-    chat = pytchat.create(video_id=vid)
+    chatIn = pytchat.create(video_id=vid)
 except pytchat.exceptions.InvalidVideoIdException:
     print(f"Invalid Video ID: {vid}.")
     sys.exit()
-if chat.is_alive():
+if chatIn.is_alive():
     mqtt_publish("None", "YouTube chat is connected to Twitch chat.")
     pass
 else:
     mqtt_publish("None", "YouTube chat could not connect to Twitch chat. Stream not alive.")
     pass
-while chat.is_alive():
-    for c in chat.get().sync_items():
+while chatIn.is_alive():
+    for c in chatIn.get().sync_items():
+        sleep(5)
         mqtt_publish(c.author.name, c.message)
         r = requests.get(f"http://{API_URL}:{API_PORT}/api/newchatmsg?author={c.author.name}&color=ffc0c0&msg={c.message}&service=YouTube")
 
